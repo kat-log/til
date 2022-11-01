0e540e2208e70129d09e4761bb18d5306cff2a25
kat-log <83802430+kat-log@users.noreply.github.com>
2022-11-01 09:34:24 +0900

    Update callback.mk

--- study/JavaScript/callback.mk
+++ study/JavaScript/callback.mk
@@ -2,6 +2,4 @@
 コールバック関数は、「仲介役」関数
 
 # 参考
-JavaScriptで学ぶWebアプリ開発の必須知識〜Node.js・Web API・Ajax・async/await〜
-
-https://www.udemy.com/course/web-application-development-with-javascript/
+[JavaScriptで学ぶWebアプリ開発の必須知識〜Node.js・Web API・Ajax・async/await〜 | Udemy](https://www.udemy.com/course/web-application-development-with-javascript/)
